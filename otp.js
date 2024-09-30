816944543e3492bc21199aa83eb309166a780276
Beriwala-Safwan <beriwalasafwan345@gmail.com>
2024-09-30 20:01:27 +0530

    Add files via upload
    this project generate random otp number

---
+++ otp.js
@@ -0,0 +1,31 @@
+// function generateOtp(length){
+// const digits="0123456789";
+// console.log(digits.length)
+// let otp="";
+// for (let i = 0; i<length; i++) {
+// const randomIndex = Math.floor(Math.random()*digits.length);
+// otp=otp+digits[randomIndex] ;
+// }
+// return otp;
+// }
+
+function generateOtp(length) {
+  const digits = "0123456789";
+  let otp = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * digits.length);
+    otp += digits[randomIndex];
+  }
+  return otp;
+}
+
+document.getElementById('generateBtn').addEventListener('click', function () {
+  const otpLength = document.getElementById('otpLength').value;
+  
+  if (otpLength && otpLength > 0) {
+    const otp = generateOtp(otpLength);
+    document.getElementById('otpDisplay').innerText = otp;
+  } else {
+    alert("Please enter a valid OTP length.");
+  }
+});
